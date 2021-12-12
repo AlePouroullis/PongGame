b194b38a1a2884bf46b0ed76bb3e84d67df72909
alexandrospouroullis <alexpouroullis123@gmail.com>
2021-12-13 01:00:29 +0200

    Collisions changed, although still bugged.

--- main.py
+++ main.py
@@ -54,19 +54,96 @@ class Game:
         if dist_between_centres.x > const.paddleWidth // 2 + ball.radius: return False
         if dist_between_centres.y > const.paddleHeight // 2 + ball.radius: return False
 
-        if dist_between_centres.x <= const.paddleWidth // 2: return True
-        if dist_between_centres.y <= const.paddleHeight // 2: return True
+        if dist_between_centres.x <= const.paddleWidth // 2:
+            return True
+        if dist_between_centres.y <= const.paddleHeight // 2:
+            return True
 
         corner_distance_sq = (dist_between_centres.x - const.paddleWidth // 2) ** 2 + (
                     dist_between_centres.y - const.paddleHeight // 2) ** 2
 
-        return corner_distance_sq <= ball.radius ** 2
+        return corner_distance_sq <= ball.radius**2
+
+    def collides_with_top_or_bottom(self, dist_between_centres):
+        if dist_between_centres.x <= const.paddleWidth // 2:
+            return True
+
+    def collides_with_side(self, dist_between_centres):
+        if dist_between_centres.y <= const.paddleHeight // 2:
+            return True
+
+    def collides_with_corner(self, dist_between_centres):
+        corner_distance_sq = (dist_between_centres.x - const.paddleWidth//2)**2 + (
+                                dist_between_centres.y - const.paddleHeight//2)**2
+
+        return corner_distance_sq <= self.ball.radius**2
 
     def check_collision_with_paddles(self):
         player_paddle_centre = Point(self.player.x + const.paddleWidth // 2, self.player.y + const.paddleHeight // 2)
         ai_paddle_centre = Point(self.ai.x + const.paddleWidth // 2, self.ai.y + const.paddleHeight // 2)
-        if self.intersects(self.ball, player_paddle_centre) or self.intersects(self.ball, ai_paddle_centre):
+        dist_between_ai_and_ball_centres = Distance(self.ball.x, self.ball.y, ai_paddle_centre.x, ai_paddle_centre.y)
+        dist_between_player_and_ball_centres = Distance(self.ball.x, self.ball.y,
+                                                        player_paddle_centre.x, player_paddle_centre.y)
+
+        if (dist_between_player_and_ball_centres.x > const.paddleWidth // 2 + self.ball.radius) and (
+                dist_between_ai_and_ball_centres.x > const.paddleWidth // 2 + self.ball.radius): return
+        if (dist_between_player_and_ball_centres.y > const.paddleHeight // 2 + self.ball.radius) and (
+                dist_between_ai_and_ball_centres.y > const.paddleHeight // 2 + self.ball.radius): return
+
+        if self.collides_with_top_or_bottom(dist_between_player_and_ball_centres):
+            self.ball.vy = -self.ball.vy
+            # if the ball collides with the top of the paddle
+            if self.ball.y > player_paddle_centre.y:
+                print("collided with the bottom of the player")
+                self.ball.y = self.player.y + const.paddleHeight + self.ball.radius
+            else:
+                print('collided with the top of the player')
+                self.ball.y = self.player.y - self.ball.radius
+
+        if self.collides_with_top_or_bottom(dist_between_ai_and_ball_centres):
+            self.ball.vy = -self.ball.vy
+            # if the ball collides with the top of the paddle
+            if self.ball.y > ai_paddle_centre.y:
+                print("collided with the bottom of ai")
+                self.ball.y = self.ai.y + const.paddleHeight + self.ball.radius
+            else:
+                print("collided with the top of the ai")
+                self.ball.y = self.ai.y - self.ball.radius
+
+        if self.collides_with_corner(dist_between_player_and_ball_centres):
+            print("collided with the corner of the player")
+            self.ball.vy = -self.ball.vy
             self.ball.vx = -self.ball.vx
+            # if the ball collides with the top corner.
+            if self.ball.y > player_paddle_centre.y:
+                self.ball.y = self.player.y - self.ball.radius
+                self.ball.x = self.player.x + const.paddleWidth + self.ball.radius
+            else:
+                self.ball.y = self.player.y + const.paddleHeight + self.ball.radius
+                self.ball.x = self.player.x + const.paddleWidth + self.ball.radius
+
+        if self.collides_with_corner(dist_between_ai_and_ball_centres):
+            print("collided with the corner of the ai")
+            self.ball.vy = -self.ball.vy
+            self.ball.vx = -self.ball.vx
+            # if the ball collides with the top corner.
+            if self.ball.y < ai_paddle_centre.y:
+                self.ball.y = self.ai.y + self.ball.radius
+                self.ball.x = self.ai.x + const.paddleWidth + self.ball.radius
+            else:
+                self.ball.y = self.ai.y + const.paddleHeight + self.ball.radius
+                self.ball.x = self.ai.x - self.ball.radius
+
+        if self.collides_with_side(dist_between_player_and_ball_centres):
+            print("collided with the side of the player")
+            self.ball.vx = -self.ball.vx
+            self.ball.x = self.player.x + const.paddleWidth + self.ball.radius
+
+        if self.collides_with_side(dist_between_ai_and_ball_centres):
+            print("collided with the side of the ai")
+            self.ball.vx = -self.ball.vx
+            self.ball.x = self.ai.x - self.ball.radius
+
 
 
 if __name__ == "__main__":
@@ -79,8 +156,8 @@ if __name__ == "__main__":
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE] and not game.isStarted:
             game.isStarted = True
-            game.ball.vx = 3
-            game.ball.vy = 5
+            game.ball.vx = 6
+            game.ball.vy = 8
 
         if game.isStarted:
             mouse_pos = pygame.mouse.get_pos()
